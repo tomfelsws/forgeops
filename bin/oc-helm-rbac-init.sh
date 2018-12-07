ab9d89d652e585686f2ff8c2b871d0f56072dc62
Tomas Felner <tomas.felner@swisssign.com>
2018-12-07 16:24:51 +0100

    openshift: changes to environment variables for Openshift commands

--- bin/oc-helm-rbac-init.sh
+++ bin/oc-helm-rbac-init.sh
@@ -4,7 +4,6 @@
 
 # See https://blog.openshift.com/getting-started-helm-openshift/
 
-#source "$(dirname $0)/../etc/oc-env.cfg"
 source "${BASH_SOURCE%/*}/../etc/oc-env.cfg"
 
 # get version of local helm client to allow deploying tiller of same version
@@ -12,6 +11,6 @@ HELM_VERSION=$( helm version --client --template "{{.Client.SemVer}}")
 
 oc process -f https://github.com/openshift/origin/raw/master/examples/helm/tiller-template.yaml -p TILLER_NAMESPACE="${TILLER_NAMESPACE}" -p HELM_VERSION=${HELM_VERSION} | oc apply -n ${TILLER_NAMESPACE} -f -
 
-oc rollout status deployment tiller
+oc rollout -n ${TILLER_NAMESPACE} status deployment tiller
 
 helm init --client-only
