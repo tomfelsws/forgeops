ee20cd29da1b9b31af75db8249b23e9eca428b78
Tomas Felner <tomas.felner@swisssign.com>
2019-01-14 15:23:08 +0100

    fix

--- bin/deploy-lib.sh
+++ bin/deploy-lib.sh
@@ -119,7 +119,7 @@ create_namespace()
         echo "=> Creating namespace \"${NAMESPACE}\""
         curl -g -X POST https://control.vshn.net/api/openshift/1/appuio%20lab/projects/?accessToken=[BigVKLOyIhxnbYuYY9zWMKR7ofatuG1G] \
           -d '{"name":"sws-foobar", \
-          "adminUids":["system:serviceaccount:sws-gitlab:gitlab"], \
+          "adminUids":["system:serviceaccount:sws-gitlab:gitlab","sws-tfelner1"], \
           "editorUids":["system:serviceaccount:sws-tiller:tiller"], \
           "productId":"dedicated:v1", \
           "customerId":"swisssign"}'
