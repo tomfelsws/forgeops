afdbc39ff2363cdae4bff29372502c637c06f165
Tomas Felner <tomas.felner@swisssign.com>
2019-01-29 16:49:57 +0100

    swissid: add helm deploy library

---
+++ bin/deploy-helm-lib.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+# Helm client version to install
+HELM_VERSION="2.12.2"
+
+install_helm()
+{
+    echo "=> Installing helm & tiller ..."
+    curl "https://kubernetes-helm.storage.googleapis.com/helm-v${HELM_VERSION}-linux-amd64.tar.gz" | tar zx
+    mv linux-amd64/helm /usr/bin/
+    mv linux-amd64/tiller /usr/bin/
+    rm -rf linux-amd64
+    helm version --client
+    tiller -version
+}
+
+setup_helm()
+{
+    echo "=> Configuring helm ..."
+    helm init --client-only
+}
+
+add_helm_repo()
+{
+    echo "=> Adding a helm repo ..."
+    helm repo add sws-chartmuseum $HELM_REPO_URL --username $HELM_REPO_USERNAME --password $HELM_REPO_PASSWORD
+    helm repo update
+}
+
+add_tiller_role()
+{
+    echo "=> Add tiller edit role to OpenShift project $NAMESPACE ..."
+    oc -n ${NAMESPACE} policy add-role-to-user edit "system:serviceaccount:${TILLER_NAMESPACE}:tiller"
+}
