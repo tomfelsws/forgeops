602cceebca00c5f1acb5f1ceceef7f2cc69ea027
Tomas Felner <tomas.felner@swisssign.com>
2019-01-30 12:55:06 +0100

    gitlab: remove unused config from swissid-gitlab

--- config/swissid-gitlab/env.sh
+++ config/swissid-gitlab/env.sh
@@ -1,23 +1,2 @@
 # Environment settings for the deployment
 # Using shell parameter expansion to parse the yaml file
-
-# remove any leading spaces
-while read line
-do
-    if [[ "$line" =~ ^domain:.*$ ]]; then
-    	DOMAIN=${line#domain:}
-    	DOMAIN=${DOMAIN// /}
-    fi
-done < $CFGDIR/common.yaml
-
-# k8s namespace to deploy in
-#NAMESPACE="foobar"
-
-# Top level domain. Do not include the leading "."
-# You can override by just providing a string here
-DOMAIN="${DOMAIN/\./}"
-
-# The components to deploy
-# Note the opendj stores are aliased as configstore,
-# userstore, ctstore - but they all use the opendj chart
-#COMPONENTS=(frconfig configstore userstore ctsstore openam amster)
