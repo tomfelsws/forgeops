dfc7e7a651ed5b3c76071da513386ec0291c9b9e
Tomas Felner <tomas.felner@swisssign.com>
2019-02-11 11:04:42 +0100

    libheader removed

--- sesam/bin/sh/setup-opendj.sh
+++ sesam/bin/sh/setup-opendj.sh
@@ -5,7 +5,6 @@
 ##############################################################################
 
 BATCH_DIR=$(cd $(dirname "$0"); pwd)
-source ${BATCH_DIR}/opendj/lib-header-opendj.sh
 source ${BATCH_DIR}/opendj/lib-shared-opendj.sh
 
 ##############################################################################
