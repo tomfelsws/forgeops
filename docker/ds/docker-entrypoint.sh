1c38cc57826f101afd4a6d3ea7503ac5e861ec2c
Tomas Felner <tomas.felner@swisssign.com>
2019-02-11 10:06:20 +0100

    swissid: postpone DS start with exec until after swissid initialization

--- docker/ds/docker-entrypoint.sh
+++ docker/ds/docker-entrypoint.sh
@@ -64,6 +64,18 @@ start() {
     exec tini -v -- ./bin/start-ds --nodetach
 }
 
+stop() {
+    echo "Stopping DS"
+    echo "Server id $SERVER_ID"
+    ./bin/stop-ds
+}
+
+start_noexec() {
+    echo "Starting DS (without exec tini)"
+    echo "Server id $SERVER_ID"
+    ./bin/start-ds
+}
+
 pause() {
     while true; do
         sleep 1000
@@ -102,8 +114,10 @@ echo "Server id is $SERVER_ID"
 case "$CMD" in
 start)
     init_container
-    start
+    start_noexec
     init_swissid
+    stop
+    start
     ;;
 restore)
     restore
