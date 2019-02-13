491f8f35bbbfe65abf72ccad01e2669f1f322fec
Tomas Felner <tomas.felner@swisssign.com>
2019-02-13 14:28:26 +0100

    removed more old sesam code

--- sesam/bin/sh/setup-opendj.sh
+++ sesam/bin/sh/setup-opendj.sh
@@ -4,33 +4,6 @@ BATCH_DIR=$(cd $(dirname "$0"); pwd)
 source ${BATCH_DIR}/opendj/lib-shared-opendj.sh
 
 ##############################################################################
-# Config Store
-##############################################################################
-
-if [ "$storeType" = "config" ]; then
-    source ${BATCH_DIR}/opendj/lib-configstore-opendj.sh
-    configStoreSetup
-fi
-
-##############################################################################
-# User Store
-##############################################################################
-
-if [ "$storeType" = "user" ]; then
-    source ${BATCH_DIR}/opendj/lib-userstore-opendj.sh
-    userStoreSetup
-fi
-
-##############################################################################
-# Token (CTS) Store
-##############################################################################
-
-if [ "$storeType" = "cts" ]; then
-    source ${BATCH_DIR}/opendj/lib-ctsstore-opendj.sh
-    ctsStoreSetup
-fi
-
-##############################################################################
 # All Stores
 ##############################################################################
 
